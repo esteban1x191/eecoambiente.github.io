60cee7f80a6719f608a1f71e675b43564e9bd740
esteban1x191 <56361267+esteban1x191@users.noreply.github.com>
2023-03-25 21:29:34 -0500

    Add files via upload

--- ideacion/ides.js
+++ ideacion/ides.js
@@ -18,3 +18,11 @@ textos.forEach((texto) => {
     });
   });
 });
+
+
+const menuBtn = document.querySelector('.hamburguer');
+const mobileMenu = document.querySelector('.mobile-menu');
+mobileMenu.classList.toggle('show');
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('show');
+});
